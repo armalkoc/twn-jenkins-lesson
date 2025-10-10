13ea585e14af5935e483dbe2495ab30265021d0f
amalkoc <armin.malkoc@insparx.com>
2025-10-10 17:41:23 +0200

    added script.groovy and moved logic to that script

--- script.groovy
+++ script.groovy
@@ -1,19 +1,12 @@
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
+def buildApp() {
+    echo "Building the application ..."
 }
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
-    }
+def testApp() {
+     echo "Testing application  ${params.selectedVersion} ..."
 }
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo "Deploying Application ..."
 }
-
 return this
