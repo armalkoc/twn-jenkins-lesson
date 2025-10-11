45a85f71324ce9470d8563edaba390c4e28c2c92
amalkoc <armin.malkoc@insparx.com>
2025-10-11 18:09:37 +0200

    added amscript.groovy and reststructured Jenkinsfile_am

---
+++ amscript.groovy
@@ -0,0 +1,19 @@
+def buildJar() {
+    echo "In this step jar file is being built ..."
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo "in this step Docker Image is being built"
+    withCredentials([usernamePassword(credentialsId: 'docker-full-pip', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t 192.168.0.112:3031/full-pipeline-lesson:am-1.0 .'
+        sh 'echo $PASS | docker login -u ${USER} --password-stdin 192.168.0.112:3031'
+        sh 'docker push 192.168.0.112:3031/full-pipeline-lesson:am-1.0'
+    }
+}
+
+def deployApp() {
+    echo "Deploying Docker Image application ..."
+}
+
+return this
