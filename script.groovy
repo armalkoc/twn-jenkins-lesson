9abead8b2134cc78e3383cabaec77d66dd3206ee
amalkoc <armin.malkoc@insparx.com>
2025-10-14 19:04:08 +0200

    commented buildJar and buildImage functions in groovy script since we defined it in SL

--- script.groovy
+++ script.groovy
@@ -1,16 +1,16 @@
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
-}
+//def buildJar() {
+//    echo 'building the application...'
+//    sh 'mvn package'
+//}
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-private-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t amalkoc/twn-demo-app:am-sl-1.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push amalkoc/twn-demo-app:am-sl-1.0'
-    }
-}
+//def buildImage() {
+//    echo "building the docker image..."
+//   withCredentials([usernamePassword(credentialsId: 'docker-hub-private-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//        sh 'docker build -t amalkoc/twn-demo-app:am-sl-1.0 .'
+//        sh 'echo $PASS | docker login -u $USER --password-stdin'
+//        sh 'docker push amalkoc/twn-demo-app:am-sl-1.0'
+//    }
+//}
 
 def deployApp() {
     echo 'deploying the application...'
